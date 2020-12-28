5d5e675fa2a6555ebf3cc9d7c0484013eab03d76
Allen <31950869+chrimerss@users.noreply.github.com>
2020-12-28 16:18:43 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /content
+
+pip -q install nose  > /dev/null 2>&1 
+
+apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
+
+apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
+
+wget xxx
+unzip *.zip
+
+pip install -r requirements.txt
+
+cd cresthh/crest && python setup.py install
