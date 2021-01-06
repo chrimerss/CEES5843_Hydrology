66742e80192f5a936e0cfbab7cfa3ba372ae9190
Allen <31950869+chrimerss@users.noreply.github.com>
2021-01-06 17:11:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ cd /content
 
 pip -q install nose  > /dev/null 2>&1 
 
-echo "Installing gdal..."
+echo "[1/4]  Installing gdal..."
 
 apt-get -q -y install python-gdal gdal-bin  > /dev/null 2>&1 
 
-echo "Installing netCDF4 ..."
+echo "[2/4]  Installing netCDF4 ..."
 apt-get -q -y install python-netcdf4  > /dev/null 2>&1 
 
-echo "uncompressing ..."
+echo "[3/4]  uncompressing ..."
 for fname in *.zip; do unzip $fname > /dev/null; done
 
-echo "install python packages..."
+echo "[4/4]  install python packages..."
 pip install -r requirements.txt > /dev/null 2>&1
 
 cd cresthh/crest && python setup.py install > /dev/null 2>&1
